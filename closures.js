9cc40d63f4029f715eb26de981154c518db541e0
Danny Harlav <danh@lolaent.com>
2016-06-01 12:03:59 +0300

    Code cleanup

--- closures.js
+++
@@ -1,8 +0,0 @@
-$(function() {
-    var nodes = document.getElementsByTagName('button');
-    for (var i = 0; i < nodes.length; i++) {
-       nodes[i].addEventListener('click', function() {
-          console.log('You clicked element #' + i);
-       });
-    }
-});
