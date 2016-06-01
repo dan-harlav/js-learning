35ad02b5b99390eeede80f7a004660fb33899c7b
Danny Harlav <danh@lolaent.com>
2016-06-01 12:27:32 +0300

    Bootstrap play

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
-$(function() {
+var bootstrap = function() {
+    var bam = function() {
+        console.log('You clicked element #' + i);
+    }
 
-});
+    var nodes = document.getElementsByTagName('button');
+    for (var i = 0; i < nodes.length; i++) {
+        nodes[i].addEventListener('click', bam);
+    }
+}
+
+$(bootstrap);
