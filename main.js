8112818867bf8a53d052de4771a5e0bd2575d51c
Danny Harlav <danh@lolaent.com>
2016-06-01 12:00:04 +0300

    master cleanup

--- main.js
+++ main.js
@@ -1,6 +1,3 @@
 $(function() {
-    // $('body').delegate('button.reset', 'click', function(event) {
-    //   var text = '<button class="start">Display users</button><button class="reset">Reset</button><hr/><div id="content"></div>';
-    //   $('body').html('').append(text);
-    // });
+
 });
