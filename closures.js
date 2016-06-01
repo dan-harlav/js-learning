7ff4cbc6e27342f9e0ba50b0bd47338b99f15ae1
Danny Harlav <danh@lolaent.com>
2016-06-01 11:32:57 +0300

    Working code

---
+++ closures.js
@@ -0,0 +1,8 @@
+$(function() {
+    var nodes = document.getElementsByTagName('button');
+    for (var i = 0; i < nodes.length; i++) {
+       nodes[i].addEventListener('click', function() {
+          console.log('You clicked element #' + i);
+       });
+    }
+});
---
+++ main.js
@@ -0,0 +1,6 @@
+$(function() {
+    // $('body').delegate('button.reset', 'click', function(event) {
+    //   var text = '<button class="start">Display users</button><button class="reset">Reset</button><hr/><div id="content"></div>';
+    //   $('body').html('').append(text);
+    // });
+});
