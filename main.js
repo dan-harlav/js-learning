c03713fd0f9bb3ebf882c7358bc2028a7b5cf60c
Danny Harlav <danh@lolaent.com>
2016-06-04 00:54:23 +0300

    Initial commit

--- main.js
+++ main.js
@@ -1,3 +1,22 @@
-$(function() {
+var generateArrayOf = function(n) {
+  var result = [];
+  for (var i = 0; i <= n; i++) {
+    result.push(i);
+  }
+  return result;
+}
 
+$(function() {
+    var list = generateArrayOf(1000);
+
+    var nextListItem = function() {
+        var item = list.shift();
+
+        if (typeof item != 'undefined') {
+          $('#content').html(item);
+            setTimeout( nextListItem, 0);
+        }
+    };
+
+    nextListItem();
 });
